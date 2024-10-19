00c0338249078becd25025c7069e08a3bf90c215
Daniil Kosyanov <kosyanov.daniil@inbox.ru>
2024-10-19 14:25:49 +0500

    Task5

--- Tyuiu.KosyanovDV.Sprint3.Task5.V30.Lib/DataService.cs
+++ Tyuiu.KosyanovDV.Sprint3.Task5.V30.Lib/DataService.cs
@@ -11,7 +11,6 @@ namespace Tyuiu.KosyanovDV.Sprint3.Task5.V30.Lib
                 for (int j = startValue2; j <= stopValue2; j++)
                 {
                     sum = sum + ((Math.Pow(x, 3) - startValue2) + x);
-                    startValue2++;
                 }
                     
             }
